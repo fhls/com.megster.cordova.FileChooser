7856b05628a6fc36742f43a4aecd4af191746369
David Xie <david.xie@complianceexperts.com>
2018-09-20 16:23:29 +1000

    Support multiple files selection
    Support multiple files selection

--- www/fileChooser.js
+++ www/fileChooser.js
@@ -1,5 +1,11 @@
 module.exports = {
     open: function (success, failure) {
-        cordova.exec(success, failure, "FileChooser", "open", []);
+        cordova.exec(success, failure, "FileChooser", "pick_file", []);
+    },
+    pickFile: function (success, fail, fileTypes) {
+        cordova.exec(success, fail, "FileChooser", "pick_file", []);
+    },
+    pickFiles: function (success, fail, fileTypes) {
+        cordova.exec(success, fail, "FileChooser", "pick_files", []);
     }
 };
